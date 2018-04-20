47cba48569f0502e5d5257b85d22907d60a47edd
Jef Oliver <jef@eljef.me>
2018-04-20 12:51:06 -0700

    Support Retrieving Current Version
    "bump-version.sh c" returns the current version.
    
    Signed-off-by: Jef Oliver <jef@eljef.me>

--- tools/bump-version.sh
+++ tools/bump-version.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 if [ -z "$1" ]; then
-    echo "bump-version.sh new_version"
+    echo "bump-version.sh [c |new_version]"
+    echo ""
+    echo "    c            get current version"
+    echo "    new_version  new version to set"
     exit 1
+elif [ "$1" == "c" ]; then
+    cat eljef/docker/__version__.py | grep 'VERSION' | \
+    sed -e 's/VERSION = //' -e "s/'//g"
 fi
 
 sed -e "s/VERSION = .*$/VERSION = '${1}'/" \
